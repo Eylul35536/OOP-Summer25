e44760d194c526bb1719ade38bc9e1bb9217d72f
Eylul35536 <eylul.kilagoz@akademiata.edu.pl>
2025-03-07 21:13:02 +0100

    add person class.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Person:
-  def __init__(self, name, age, adress)
-  self.name = name
-  self.age = age
-  self.adress = adress
+  def __init__(self, name, age, adress):
+    self.name = name
+    self.age = age
+    self.adress = adress
 
 p1 = Person("Eylul" , 21 , "Jana pawla 61" )
 print(p1.name , p1.age, p1.adress)
