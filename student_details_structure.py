4a77981353ca198169ae56af19137d05cdc32e63
Eylul35536 <eylul.kilagoz@akademiata.edu.pl>
2025-03-13 14:30:46 +0100

    student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,14 @@
+student1 = {
+'first_name'= 'Eylul'
+'last_name'= 'Kilagoz'
+'index_number'= '35536'
+'nationality'= 'Turkish'
+'starting_date'= '15.01.2024'
+'courses'= ['Object-Oriented Programming' ,  'Mathematics']
+}
+print(student1['first_name'])
+print(student1['last_name'])
+print(student1['index_number'])
+print(student1['nationality'])
+print(student1['starting_date'])
+print(student1['courses']) 
