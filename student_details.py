8a351521044171c261b49d79f6b85becc42d2a85
Eylul35536 <eylul.kilagoz@akademiata.edu.pl>
2025-03-13 14:13:55 +0100

    student_details.py

--- student_details.py
+++ student_details.py
@@ -3,4 +3,4 @@ last_name= "Kilagoz"
 index_number= "35536"
 nationality= "Turkish"
 starting_date= "15.01.2024"
-courses= { "Object-Oriented Programming" ,  "Mathematics" }
+courses= ["Object-Oriented Programming" ,  "Mathematics"]
