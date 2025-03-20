b30c5838d18542d912069f260b784be40ebc84d5
Eylul35536 <eylul.kilagoz@akademiata.edu.pl>
2025-03-20 14:26:42 +0100

    animals.py

---
+++ animals.py
@@ -0,0 +1,39 @@
+animal_1 = {
+  'name' : 'Cat' ,
+  'group' : 'Mamals' ,
+  'number_of_legs' : 4 ,
+  'number_of_tales' : 1 ,
+  'skills' : ['Running' , 'Jumping' , 'Licking'] 
+}
+
+animal_2 = {
+  'name' : 'Lion',
+  'group' : 'Mamals' ,
+  'number_of_legs' : 4 ,
+  'number_of_tales' : 1 ,
+  'skills' : ['Hunting' , 'Night Vision' , 'Smelling'] 
+}
+
+animal_3 = {
+  'name' : 'Crocodiles' ,
+  'group' : 'Reptiles' ,
+  'number_of_legs' : 4 ,
+  'number_of_tales' : 1 ,
+  'skills' : ['Sound Perception' , 'Swimming' , 'Biting'] 
+}
+
+animal_4 = {
+  'name' : 'Parrot' ,
+  'group' : 'Birds' ,
+  'number_of_legs' : 2 ,
+  'number_of_tales' : 0 ,
+  'skills' : ['Climbing' , 'Talking' , 'Flying'] 
+}
+
+animal_5 = {
+  'name' : 'Salmon' ,
+  'group' : 'Fish' ,
+  'number_of_legs' : 0 ,
+  'number_of_tales' : 1 ,
+  'skills' : ['Swimming' , 'Flexible'] 
+}  
