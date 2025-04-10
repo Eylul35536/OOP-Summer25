b987c264a9e49d2748ee04b69af672a3d7d58ac9
Eylul35536 <eylul.kilagoz@akademiata.edu.pl>
2025-04-10 16:58:09 +0200

    Update pyhton_variables.py

--- w3_school/pyhton_variables.py
+++ w3_school/pyhton_variables.py
@@ -66,3 +66,38 @@ print(x)
 print(y)
 print(z)
 
+### Global Variables 
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+## Global Keywords 
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+myfunc()
