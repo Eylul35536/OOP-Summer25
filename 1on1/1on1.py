213366affadb2a8771d8541aed9708af4bbb714a
Eylul35536 <eylul.kilagoz@akademiata.edu.pl>
2025-04-10 13:04:02 +0000

    Add new file

---
+++ 1on1/1on1.py
@@ -0,0 +1,25 @@
+print("Eylul")
+
+x = 5
+y = 5.7
+
+
+#a 
+print(x+y)
+
+result = x + y 
+print(result)
+print(type(result))
+
+animals = ["cat" , "dog" , "bird"]
+for x in animals :
+ print(x)
+
+ computer1 = {
+  "device" : "PC",
+  "color" : "black",
+  "type" : "laptop",
+  "year" : 2025
+ }
+
+print(computer1["color"])
---
+++ deneme.py/denemewada_1.py
@@ -0,0 +1,24 @@
+print('Eylul')
+
+x = "Hello"
+y = 5
+z = 5.25
+
+print(type(y))
+
+fruits = ["banana", "cherry" , "kiwi"]
+print(fruits[2])
+
+my_dict = {
+    "name" : "Eylul",
+    "age"  : 21
+}
+
+a = 8
+b = "eylul"
+c = 5.8
+
+##for loop 
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
